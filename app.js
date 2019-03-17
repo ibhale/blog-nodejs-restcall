ac4667736b10cefd85fb3aad24f35931200ac592
unknown <ishanbhale1989@gmail.com>
2019-03-17 14:50:43 -0500

    merged scripts into one

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
+// nodejs script to make call to REST endpoints, this can be integrated in Node.js RESTful service or bash scripts or consumed directly as a REST client
+
 const fs = require('fs');
 var request = require('request');
 var FormData = require('form-data');
 
 var form = new FormData();
 form.append("folder_id", "0");
-form.append("file", fs.createReadStream('IMG_4318.JPG'));
+form.append("file", fs.createReadStream('userdata.csv'));
 
+// post method rest call to upload a file
 form.getLength(function(err, length){
   if (err) {
     return requestCallback(err);
   }
 
   var r = request.post("http://localhost:8080/profile/file", requestCallback);
-  r._form = form;     
+  r._form = form;
 
 });
+// get method rest call to retrieve the data
+var r = request.get("http://localhost:8080/profile/contact", requestCallback);
 
 function requestCallback(err, res, body) {
   console.log(body);
--- application.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-var request = require('request');  
-var r = request.get("http://localhost:8080/profile/contact", requestCallback);
-function requestCallback(err, res, body) {
-  console.log(body);
-}
